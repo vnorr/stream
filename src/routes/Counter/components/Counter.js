8c58b7e92aac0c7291a8b71d1c55e290f81c491a
Victor Norrgård <victor.norrgard@carable.com>
2018-03-23 10:55:16 +0100

    init

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,22 +1,76 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import MessageBoard from './MessageBoard'
+import './counter.scss'
 
-export const Counter = ({ counter, increment, doubleAsync }) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>Counter: {counter}</h2>
-    <button className='btn btn-primary' onClick={increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-secondary' onClick={doubleAsync}>
-      Double (Async)
-    </button>
-  </div>
-)
-Counter.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increment: PropTypes.func.isRequired,
-  doubleAsync: PropTypes.func.isRequired,
+const stream = require('getstream')
+const client = stream.connect('n2wcamdjmvt4', null, '35635')
+
+export default class componentName extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      feed: {},
+      form : {
+        message: ''
+      }
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSendMessage = this.handleSendMessage.bind(this)
+  }
+
+  handleInputChange (e) {
+    const { form } = this.state
+    const input = e.target.value
+    const index = e.target.name
+
+    form[index] = input
+    this.setState({ form })
+  }
+
+  handleSendMessage () {
+    const stream = client.feed('user', 'eric', 'du5zqrTMZ8fiJXdKt0JzYBVoyJs')
+    const { form, feed } = this.state
+    console.log(form.message)
+
+    stream.addActivity({
+      actor: 'eric',
+      verb: 'tweet',
+      tweet: form.message,
+      object: feed.results.length + 1
+    }).then(() => {
+      this.getStream()
+    })
+  }
+
+  getStream () {
+    const ericFeed = client.feed('user', 'eric', 'du5zqrTMZ8fiJXdKt0JzYBVoyJs')
+    ericFeed.subscribe((data) => {
+      this.setState({ feed: data })
+    })
+
+    ericFeed.get().then((data) => {
+      this.setState({ feed: data })
+    })
+  }
+
+  componentDidMount () {
+    this.getStream()
+  }
+
+  render () {
+    const { feed } = this.state
+
+    return (
+      <div className='Container'>
+        <div>
+          <MessageBoard
+            name='Activity'
+            handleInputChange={this.handleInputChange}
+            handleSendMessage={this.handleSendMessage}
+            feed={feed}
+          />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default Counter
---
+++ src/routes/Counter/components/MessageBoard.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './MessageBoard.scss'
+
+export default class MessageBoard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    const { handleInputChange, handleSendMessage, feed } = this.props
+    console.log(feed)
+    let messages = []
+
+    if (feed.results) {
+      feed.results.forEach((message, i) => {
+        if (message.verb === 'tweet') {
+          const key = `tweet_${i}`
+          messages.push(<span className='Tweet' key={key}> {message.tweet}</span>)
+        }
+      })
+    }
+
+    return (
+      <div>
+        <h3>{name}</h3>
+        <div className='ContainerWrap'>
+          {messages}
+        </div>
+        <input onChange={handleInputChange} type='text' name='message' />
+        <button onClick={handleSendMessage} type='submit'>Send</button>
+      </div>
+    )
+  }
+}
+
+MessageBoard.propTypes = {
+  handleInputChange: PropTypes.func,
+  handleSendMessage: PropTypes.func,
+  feed: PropTypes.object
+}
